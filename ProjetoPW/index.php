9625b9a1dac4f24a2dc100922c6b58cdfaea2dc9
Michael Brunelli <88911788+michaelbrunelli@users.noreply.github.com>
2022-08-12 11:15:42 -0300

    Add files via upload

---
+++ ProjetoPW/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Projeto PHP</title>
+    </head>
+    <body>
+        <table width="50%" cellspacing="1" cellspacing="2" border="0" bgcolor="#047AC4" align="center">
+                <tr>
+                    <td align="center" bgcolor="#047AC4"><font color="#FFFFFF" size=1 face="verdana, arial, helvetica"><b>
+                        <h3><img src="\ProjetoPW\botoes\imgEmpresa.jpg" title="Empresa Softron"></h3>
+                            </b>
+                                </font>
+                                        </td>
+                </tr>
+                <tr>
+                    <td bgcolor="#047AC4">
+                        <table width="95%" cellspacing="1" cellspacing="1" border="0" align="center">
+                            <tr>
+                                <td align="center"><font face="verdana, arial, helvetica" size=1>A ferramenta de cadastro e controle de Estoque</font></td>
+                            </tr>
+
+                        </table>
+                    <table width="50%" cellspacing="1" cellspacing="3" border="0" bgcolor="#047AC4" align="center">
+                        <tr>
+                            <td bgcolor="#FFFFFF">
+                                <table>
+                                    <tr>
+                                        <td><font face="verdana, arial, helvetica" size=1><A href="cadastro_cliente.php">
+                                            <img border="0" src="\ProjetoPW\botoes\cad_Cliente.jpg" title="Controle de Estoque"></A></font></td>
+                                        <td><font face="verdana, arial, helvetica" size=1><A href="cadastro_fornecedor.php">
+                                            <img border="0" src="\ProjetoPW\botoes\cad_Fornecedor.jpg" title="Cadastro de Cliente e Fornecedor"></A></font></td>
+                                        <td><font face="verdana, arial, helvetica" size=1><A href="cadastro_produto.php">
+                                            <img border="0" src="\ProjetoPW\botoes\cad_Produto.jpg" title="Cadastro Produto"></A></font></td>
+                                        <td><font face="verdana, arial, helvetica" size=1><A href="pdv.php">
+                                            <img border="0" src="\ProjetoPW\botoes\pdv.jpg" title="PDV"></A></font></td>
+                                        <td><font face="verdana, arial, helvetica" size=1><A href="estoque.php">
+                                            <img border="0" src="\ProjetoPW\botoes\cad_Estoque.jpg" title="Estoque"></A></font></td>
+                                    </tr>
+                                </table>
+
+                            </td>
+                        </tr>
+
+                    </table>
+                    </td>
+                </tr>
+        </table>
+    </body>
+</html>
