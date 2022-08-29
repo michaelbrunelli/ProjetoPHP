0658e1b731bc44f31bf790704cdf0bb4855f1941
Michael Brunelli <88911788+michaelbrunelli@users.noreply.github.com>
2022-08-29 19:32:44 -0300

    Update cadastro_fornecedor.php

---
+++ ProjetoPW/cadastro_fornecedor.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <title>Tela Cadastro do Fornecedor</title>
+    </head>
+    <body>
+        
+    <div align="center">
+        <table width="50%">
+        <form action="">
+            <div>
+                <h1>CADASTRO FORNECEDOR</h1>
+                <br>
+                <label for="formGroupExampleInput" class="form-label">Nome da Empresa:</label>
+                <input type="text" placeholder="Razão Social" aria-label="Razão Social">
+                </div>
+
+            </div>
+            <br>
+            <div class="mb-3"  class="text-md-left">
+            <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">CNPJ:</label>
+                <input type="text" id="formGroupExampleInput2" placeholder="Digite seu CNPJ">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">Endereço:</label>
+                <input type="text" id="formGroupExampleInput2" placeholder="Digite seu Endereço">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput" class="form-label">Número:</label>
+                <input type="text" id="formGroupExampleInput" placeholder="Digite seu Número">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">Bairro:</label>
+                <input type="text" id="formGroupExampleInput2" placeholder="Digite seu Bairro">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput" class="form-label">Cidade:</label>
+                <input type="text" id="formGroupExampleInput" placeholder="Digite sua Cidade">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">CEP:</label>
+                <input type="text" id="formGroupExampleInput2" placeholder="Digite seu CEP">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput" class="form-label">Celular:</label>
+                <input type="number" id="formGroupExampleInput" placeholder="(xx) xxxxx-xxxx">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">E-mail:</label>
+                <input type="text" id="formGroupExampleInput2" placeholder="Digite seu E-mail">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput" class="form-label">Observações:</label>
+                <input type="text" id="formGroupExampleInput" placeholder="Escreva sua observação">
+            </div>
+        </form>
+        </table>
+</div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    </body>
+    </html>
