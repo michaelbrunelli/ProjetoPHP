0c3806a8b550c6c04af9d40d2cc602bc59a345cb
Michael Brunelli <88911788+michaelbrunelli@users.noreply.github.com>
2022-08-29 19:32:59 -0300

    Update cadastro_produto.php

---
+++ ProjetoPW/cadastro_produto.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <title>Tela Cadastro de Produtos</title>
+    </head>
+    <body>
+        
+    <div align="center">
+        <table width="50%">
+        <form action="">
+            <div>
+                <h1>CADASTRO PRODUTO</h1>
+                <label class="visually-hidden" for="inlineFormSelectPref">Nome/Razão Social</label>
+                <br>
+            <div class="mb-3"  class="text-md-left">
+            <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">Nome do Produto:</label>
+                <input type="text" id="formGroupExampleInput2" placeholder="Digite o nome do produto">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">Código do Produto:</label>
+                <input type="text" id="formGroupExampleInput2" placeholder="Digite o código do produto">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">Marca do Produto:</label>
+                <input type="text" id="formGroupExampleInput2" placeholder="Digite a marca do produto">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">Categoria:</label>
+                <input type="text" id="formGroupExampleInput2" placeholder="Digite a sua categoria">
+                </div>
+                <div class="mb-3">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput" class="form-label">Quantidade:</label>
+                <input type="number" id="formGroupExampleInput" placeholder="xxxx">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">Valor de Custo:</label>
+                <input type="text" id="formGroupExampleInput2" placeholder="Digite o valor de custo ($)">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">Valor de Venda:</label>
+                <input type="text" id="formGroupExampleInput2" placeholder="Digite o valor de venda ($)">
+                </div>
+                <div class="mb-3">
+                <label for="formGroupExampleInput" class="form-label">Descrição:</label>
+                <input type="text" id="formGroupExampleInput" placeholder="Descreva seu produto">
+            </div>
+        </form>
+        </table>
+</div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    </body>
+    </html>
