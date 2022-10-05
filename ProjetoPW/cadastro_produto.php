33475e216b2011f7914fdae45642408bd7224028
Michael Brunelli <88911788+michaelbrunelli@users.noreply.github.com>
2022-10-04 21:35:44 -0300

    Update cadastro_produto.php

--- ProjetoPW/cadastro_produto.php
+++ ProjetoPW/cadastro_produto.php
@@ -4,8 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" 
-        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <link rel="stylesheet" href="teupai.css">
         <title>Tela Cadastro de Produtos</title>
         <form action="dbCad_Produto.php" method="POST">
     </head>
@@ -15,7 +14,7 @@
         <table width="50%">
         <form action="">
             <div>
-                <h1>CADASTRO PRODUTO</h1>
+                <h1>CADASTRO PRODUTO</h1><br>
             <div class="mb-3">
                 <label for="formGroupExampleInput2" class="form-label">Nome do Produto:</label>
                 <input type="text" name="nproduto" placeholder="Digite o nome do produto">
@@ -26,7 +25,5 @@
         </form>
         </table>
 </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" 
-        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     </body>
     </html>
