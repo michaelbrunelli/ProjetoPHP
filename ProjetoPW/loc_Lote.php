f40bb36bdd2e5eeedaa9af9e91d6c738a65ccd66
Michael Brunelli <88911788+michaelbrunelli@users.noreply.github.com>
2022-10-25 22:13:26 -0300

    Update loc_Lote.php

--- ProjetoPW/loc_Lote.php
+++ ProjetoPW/loc_Lote.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="suamae.css">
     <title>Localizador do Lote</title>
 </head>
 <body>
@@ -11,12 +12,13 @@
         
 
 
-<table>
-      <tr bgcolor=#0000FF>
-       <td><font align= center  face=\"verdana, arial, helvetica\" size=1>Produto</td>
-        <td><font align= center  face=\"verdana, arial, helvetica\" size=1>QTD</td>
-        <td><font align= center  face=\"verdana, arial, helvetica\" size=1>Valor</td>
-        <td><font align= center  face=\"verdana, arial, helvetica\" size=1>Unidade Medida</td> 
+<table width="150px" border="3" align="center">
+      <tr bgcolor=#101474>
+      <td><font align= center  face=\"verdana, arial, helvetica\">Código</td>
+       <td><font align= center  face=\"verdana, arial, helvetica\">Produto</td>
+        <td><font align= center  face=\"verdana, arial, helvetica\">QTD</td>
+        <td><font align= center  face=\"verdana, arial, helvetica\">Valor</td>
+        <td><font align= center  face=\"verdana, arial, helvetica\">Unidade Medida</td> 
 <?php
 
 include "mysqlconecta.php";
@@ -24,27 +26,29 @@ include "mysqlconecta.php";
 $busca = $_POST["busca"];
 
 
-$query = mysqli_query($conexao, "SELECT p.pro_nome, e.est_qtd, e.est_valor, e.est_um FROM cad_produtos p, estoque e where p.pro_cod = e.est_id_pro and p.pro_nome like \"%$busca%\" group by 1");
+$query = mysqli_query($conexao, "SELECT e.est_id, p.pro_nome, e.est_qtd, e.est_valor, e.est_um FROM cad_produtos p, estoque e where p.pro_cod = e.est_id_pro and p.pro_nome like \"%$busca%\" group by 1");
 while ($saida = mysqli_fetch_array($query))
     {
-        $produto = $saida[0];
-        $qtd = $saida[1];
-        $valor = $saida[2];
-        $um = $saida[3];
+        $id = $saida[0];
+        $produto = $saida[1];
+        $qtd = $saida[2];
+        $valor = $saida[3];
+        $um = $saida[4];
 
-        echo ("<tr bgcolor=#4169E1");
+        echo ("<tr bgcolor=	(79,79,79) ");
         echo ("</td>");
-        echo ("<td><font align= center  face=\"verdana, arial, helvetica\" size=1>" .$produto); echo("</td>");
-        echo ("<td><font align= center  face=\"verdana, arial, helvetica\" size=1>" .$qtd); echo("</td>");
-        echo ("<td><font align= center  face=\"verdana, arial, helvetica\" size=1>" .$valor); echo("</td>");
-        echo ("<td><font align= center  face=\"verdana, arial, helvetica\" size=1>" .$um); echo("</td>"); 
+        echo ("<td><font align= center  face=\"verdana, arial, helvetica\">" .$id); echo("</td>");
+        echo ("<td><font align= center  face=\"verdana, arial, helvetica\">" .$produto); echo("</td>");
+        echo ("<td><font align= center  face=\"verdana, arial, helvetica\">" .$qtd); echo("</td>");
+        echo ("<td><font align= center  face=\"verdana, arial, helvetica\">" .$valor); echo("</td>");
+        echo ("<td><font align= center  face=\"verdana, arial, helvetica\">" .$um); echo("</td>"); 
     }
 
     echo ("</tr>");
     mysqli_close ($conexao);
 ?>
+
 </tr>
 </table>
 </body>
 </html>
-
